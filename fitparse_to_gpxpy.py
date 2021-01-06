0dbe960f70bf0a0b5b14f0ee0f84c8ebfe597fcb
Aaron <ondrsalf@mail.de>
2021-01-06 23:26:22 +0100

    now support laps/segments
    TODO: currently, this creates a blank segment and only appends it to the GPX if it reads a 'lap' message (at the end of a lap)
    What happens if there is no 'lap' message. Can we assume there always is one?

--- fitparse_to_gpxpy.py
+++ fitparse_to_gpxpy.py
@@ -16,25 +16,34 @@ def parse_fit_to_gpx(fitname) -> gpxpy.gpx.GPX:
     gpx.tracks.append(gpxTrack)
 
     gpxSegment = gpxpy.gpx.GPXTrackSegment()
-    gpxTrack.segments.append(gpxSegment)
+    #gpxTrack.segments.append(gpxSegment)
 
-    for record in fitfile.get_messages("record"):
-        gpxTrackPoint = gpxpy.gpx.GPXTrackPoint()
+    for message in fitfile.messages:
+        if message.name == "lap":
+            gpxTrack.segments.append(gpxSegment) # append the current TrackSegment to the GPX
+            gpxSegment = gpxpy.gpx.GPXTrackSegment() # and start a new one for the records to follow
+            # TODO: What happens if there is no 'lap' message at the end of the .fit file?
 
-        for data in record.fields:
-            if data.name == "position_long":
-                gpxTrackPoint.longitude = semicircles_to_degrees(data.value)
-            if data.name == "position_lat":
-                gpxTrackPoint.latitude  = semicircles_to_degrees(data.value)
-            if data.name == "timestamp":
-                gpxTrackPoint.time      = data.value
-            if data.name == "altitude":
-                gpxTrackPoint.elevation = round(data.value, 8)
+        if message.name == "record":
+            gpxTrackPoint = gpxpy.gpx.GPXTrackPoint()
 
-        if not (gpxTrackPoint.latitude and gpxTrackPoint.longitude):
-            continue
+            for data in message.fields:
+                if data.name == "position_long" and data.value != None:
+                    gpxTrackPoint.longitude = semicircles_to_degrees(data.value)
 
-        gpxSegment.points.append(gpxTrackPoint)
+                if data.name == "position_lat" and data.value != None:
+                    gpxTrackPoint.latitude  = semicircles_to_degrees(data.value)
+                
+                if data.name == "timestamp":
+                    gpxTrackPoint.time      = data.value
+                
+                if data.name == "altitude":
+                    gpxTrackPoint.elevation = round(data.value, 8)
+
+            if not (gpxTrackPoint.latitude and gpxTrackPoint.longitude):
+                continue
+
+            gpxSegment.points.append(gpxTrackPoint)
 
     return gpx
 
